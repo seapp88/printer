c8b68263879f355b5e8793c31b962e4422f77494
seapp <seapp88@icloud.com>
2020-09-23 12:45:04 +0300

    init

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const printer = require("zebra-printer");
+const printer = require("printer");
 const util = require('util');
 console.log("installed printers:\n"+util.inspect(printer.getPrinters(), {colors:true, depth:10}));
