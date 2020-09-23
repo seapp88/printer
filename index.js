275ba12c1ebbadb661946f78d4ac0367b8c13978
seapp <seapp88@icloud.com>
2020-09-23 13:09:00 +0300

    init

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const printer = require("printer");
 const util = require('util');
+const io = require('socket.io-client');
+
+const socket = io('http://10.10.10.10:3000');
+
+socket.on('connect', () => {
+    console.log(socket.connected); // true
+});
+
+socket.on('disconnect', () => {
+    console.log(socket.connected); // false
+});
+
+
 console.log("installed printers:\n"+util.inspect(printer.getPrinters(), {colors:true, depth:10}));
